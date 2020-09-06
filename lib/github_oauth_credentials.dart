691a7856e8e0bd7a01336deff9ac186b3f152fd7
Andy Van Pelt <andyv@trabus.com>
2020-09-06 12:30:30 -0700

    Initial commit
    https://codelabs.developers.google.com/codelabs/flutter-github-graphql-client/index.html

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '62d4e4a82544223f072e';
+const githubClientSecret = 'd7d3cacfdf56651eaad47f0474f6e19850dd031b';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
